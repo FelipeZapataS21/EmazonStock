cc66cec913729040e5afaf1acc72ff4c4373f7f5
Felipe Zapata <zapatasierrafmail.com>
2024-09-01 22:11:23 -0500

    SonarLint: mejoras

--- src/main/java/com/example/emazonstock/infrastructure/utils/InfrastructureConstants.java
+++ src/main/java/com/example/emazonstock/infrastructure/utils/InfrastructureConstants.java
@@ -1,6 +1,11 @@
 package com.example.emazonstock.infrastructure.utils;
 
 public class InfrastructureConstants {
+
+    private InfrastructureConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final String EXCEPTION_CATEGORY_NOT_FOUND = "No Category was found with that name";
     public static final String EXCEPTION_CATEGORY_ALREADY_EXISTS = "There is already a category with that name";
     public static final String EXCEPTION_NO_DATA_FOUND = "No data found for the requested petition";
